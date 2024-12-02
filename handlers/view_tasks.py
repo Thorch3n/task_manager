468a7e0a62c1e026b14d5ecc831f7f9ff308e23d
Thorch3n <relax.041998@gmail.com>
2024-12-02 16:23:27 +0500

    Объединение по функционалу

--- handlers/view_tasks.py
+++ handlers/view_tasks.py
@@ -5,3 +5,13 @@ def view_tasks(manager):
     else:
         for task in tasks:
             print(f"\n{task.to_dict()}")
+
+
+def view_tasks_by_category(manager):
+    category = input(f"\nВведите категорию: ")
+    tasks = [task for task in manager.tasks if task.category == category]
+    if not tasks:
+        print(f"\nНет задач в категории '{category}'.")
+    else:
+        for task in tasks:
+            print(f"\n{task.to_dict()}")
--- handlers/view_tasks_by_category.py
+++
@@ -1,8 +0,0 @@
-def view_tasks_by_category(manager):
-    category = input(f"\nВведите категорию: ")
-    tasks = [task for task in manager.tasks if task.category == category]
-    if not tasks:
-        print(f"\nНет задач в категории '{category}'.")
-    else:
-        for task in tasks:
-            print(f"\n{task.to_dict()}")
